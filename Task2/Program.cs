8a0801a124797b69ebdd8d16e99c91de05b06f6e
DESKTOP-LLAL946\devil <mr.alkov@mail.ru>
2022-08-07 20:49:38 +0700

    endtask1

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.Write("Введите целое число: ");
+int a = Convert.ToInt32(System.Console.ReadLine());
+
+if( a == 1) System.Console.WriteLine("Понедельник");
+else if( a == 2) System.Console.WriteLine("Вторник");
+else if( a == 3) System.Console.WriteLine("Среда");
+else if( a == 4) System.Console.WriteLine("Четверг");
+else if( a == 5) System.Console.WriteLine("Пятница");
+else if( a == 6) System.Console.WriteLine("Суббота");
+else if( a == 7) System.Console.WriteLine("Воскресенье");
+else System.Console.WriteLine("Число вне диапазона от 1 до 7");
