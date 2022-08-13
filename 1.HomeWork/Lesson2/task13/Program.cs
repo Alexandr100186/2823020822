934a255d0ed1fafb4cddec8fc693779b2264faef
DESKTOP-LLAL946\devil <mr.alkov@mail.ru>
2022-08-13 22:11:36 +0700

    endhome13

--- 1.HomeWork/Lesson2/task13/Program.cs
+++ 1.HomeWork/Lesson2/task13/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+//  что третьей цифры нет. Решить без использования строк.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < -99 || num > 99)
+{
+    int result = ((num / 100) % 10);
+    int x = Math.Abs(result);
+    Console.WriteLine($": {x}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
