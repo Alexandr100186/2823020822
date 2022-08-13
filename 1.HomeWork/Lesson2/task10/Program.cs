0ff3245791165df22acd1a354878cd70fc4e7dea
DESKTOP-LLAL946\devil <mr.alkov@mail.ru>
2022-08-13 21:05:32 +0700

    endhome10

--- 1.HomeWork/Lesson2/task10/Program.cs
+++ 1.HomeWork/Lesson2/task10/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа. Решить без использования строк.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > -1000 && num < -99 || num < 1000 && num > 99)
+{
+    int result = (num / 10)- (num / 100 * 10);
+    int x = Math.Abs(result);
+    Console.WriteLine($": {x}");
+}
+else
+{
+    Console.WriteLine("Введено не верное значение");
+}
