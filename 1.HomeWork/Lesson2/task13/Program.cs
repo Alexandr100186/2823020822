96c67c87efab861b7821ac618f43fcf1fff3af0d
DESKTOP-LLAL946\devil <mr.alkov@mail.ru>
2022-08-13 23:41:54 +0700

    endhome130

--- 1.HomeWork/Lesson2/task13/Program.cs
+++ 1.HomeWork/Lesson2/task13/Program.cs
@@ -4,15 +4,33 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+// Третья цифра справа:
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num < -99 || num > 99)
+// {
+//     int result = ((num / 100) % 10);
+//     int x = Math.Abs(result);
+//     Console.WriteLine($": {x}");
+// }
+// else
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+
+// Третья цифра слева:
+
 Console.Write("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-if (num < -99 || num > 99)
-{
-    int result = ((num / 100) % 10);
-    int x = Math.Abs(result);
-    Console.WriteLine($": {x}");
-}
-else
-{
-    Console.WriteLine("Третьей цифры нет");
-}
+int n = Convert.ToInt32(Console.ReadLine());
+    if (n < -99 || n > 99)
+    {
+         while (n > 999 || n < - 999)
+        n = n / 10;
+        int x = Math.Abs(n % 10);
+        Console.WriteLine($"{x}");
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
