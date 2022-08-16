553ad4c9295c48225fe5e45748841c88b7bc46a4
DESKTOP-LLAL946\devil <mr.alkov@mail.ru>
2022-08-17 01:18:31 +0700

    endClass19

--- 1.HomeWork/Lesson3/hometask19/Program.cs
+++ 1.HomeWork/Lesson3/hometask19/Program.cs
@@ -3,20 +3,35 @@
 // 23432 -> да
 // 12821 -> да
 
-Console.Write("Введите проверяемое на палиндромность число: ");
-string? number = Console.ReadLine();
+// Console.Write("Введите проверяемое на палиндромность число: ");
+// string? number = Console.ReadLine();
 
-void CheckingNumber(string number)
-{
-    if (number[0] == number[4] || number[1] == number[3])
-    {
-        Console.WriteLine($"{number} -> да");
-    }
-    else Console.WriteLine($"{number} -> нет");
-}
+// void CheckingNumber(string number)
+// {
+//     if (number[0] == number[4] || number[1] == number[3])
+//     {
+//         Console.WriteLine($"{number} -> да");
+//     }
+//     else Console.WriteLine($"{number} -> нет");
+// }
 
-if (number!.Length == 5)
+// if (number!.Length == 5)
+// {
+//     CheckingNumber(number);
+// }
+// else Console.WriteLine($"Введено некорректное значение");
+
+Console.WriteLine("Введите проверяемое на палиндромность число: ");
+int num = Convert.ToInt32(Console.ReadLine()), rev = 0, dig, n;
+
+n = num;
+while (num > 0)
 {
-    CheckingNumber(number);
+    dig = num % 10;
+    rev = rev * 10 + dig;
+    num = num / 10; 
+    if (rev == n)
+    break ;
 }
-else Console.WriteLine($"Введено некорректное значение");
+if (n == rev) Console.WriteLine("да");
+else Console.WriteLine("Нет");
