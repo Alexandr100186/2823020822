e424fe2c0995bbdc5d77b3a0f9c84cf58c620c04
DESKTOP-LLAL946\devil <mr.alkov@mail.ru>
2022-08-16 23:18:03 +0700

    endClass23

--- 1.HomeWork/Lesson3/hometask23/Program.cs
+++ 1.HomeWork/Lesson3/hometask23/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите значение: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+if (num > 0)
+{
+    while (i <= num)
+    {
+        int q = i * i * i;
+        Console.WriteLine($"{i}\t{q}");
+        i++;
+    }
+}
+else
+{
+    Console.WriteLine("Введите значение больше 0!");
+}
