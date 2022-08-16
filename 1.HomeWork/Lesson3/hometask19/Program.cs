60c45f65e6126e6745de4140c98f7d03020f09a1
DESKTOP-LLAL946\devil <mr.alkov@mail.ru>
2022-08-17 01:19:53 +0700

    endhome19

--- 1.HomeWork/Lesson3/hometask19/Program.cs
+++ 1.HomeWork/Lesson3/hometask19/Program.cs
@@ -3,24 +3,6 @@
 // 23432 -> да
 // 12821 -> да
 
-// Console.Write("Введите проверяемое на палиндромность число: ");
-// string? number = Console.ReadLine();
-
-// void CheckingNumber(string number)
-// {
-//     if (number[0] == number[4] || number[1] == number[3])
-//     {
-//         Console.WriteLine($"{number} -> да");
-//     }
-//     else Console.WriteLine($"{number} -> нет");
-// }
-
-// if (number!.Length == 5)
-// {
-//     CheckingNumber(number);
-// }
-// else Console.WriteLine($"Введено некорректное значение");
-
 Console.WriteLine("Введите проверяемое на палиндромность число: ");
 int num = Convert.ToInt32(Console.ReadLine()), rev = 0, dig, n;
 
