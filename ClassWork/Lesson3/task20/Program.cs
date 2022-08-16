c7d7d3ecf57dcfba49f63d984fea0e668c421e68
DESKTOP-LLAL946\devil <mr.alkov@mail.ru>
2022-08-16 20:10:25 +0700

    endClass201

--- ClassWork/Lesson3/task20/Program.cs
+++ ClassWork/Lesson3/task20/Program.cs
@@ -16,3 +16,24 @@ int y2 = Convert.ToInt32(Console.ReadLine());
 
 double result = Math.Sqrt((x2 - x1)*(x2 - x1)+(y2 - y1)*(y2 - y1));
 Console.WriteLine("Расстояние между точками равно: {0: #.##}", result);
+
+// Через метод
+// double Distance(int xc1, int yc1, int xc2, int yc2)
+// {
+// return Math.Sqrt((xc2 - xc1) * (xc2 - xc1) + (yc2 - yc1) * (yc2 - yc1));
+
+// }
+
+// Console.WriteLine("1 точка ");
+// Console.Write("Введите координату X: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату Y: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("2 точка ");
+// Console.Write("Введите координату X: ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату Y: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+
+// double res = Distance(x1, y1, x2, y2);
+// Console.WriteLine(Math.Round(res,5));
