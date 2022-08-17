a53a74e2030d43f4fed3406cb83bba638e56b63a
DESKTOP-LLAL946\devil <mr.alkov@mail.ru>
2022-08-17 18:33:09 +0700

    endlection30

--- ClassWork/Lesson3/lection3/Program.cs
+++ ClassWork/Lesson3/lection3/Program.cs
@@ -1,99 +1,99 @@
-﻿// Вид 1 (которые получают данные но не возвращают)
+﻿// // Вид 1 (которые получают данные но не возвращают)
 
-void Method1()
-{
-    Console.WriteLine("Автор...");
-}
-//Method1();
+// void Method1()
+// {
+//     Console.WriteLine("Автор...");
+// }
+// //Method1();
 
-//Вид 2
+// //Вид 2
 
-void Method21(string msg, int count)
-{
-    int i = 0;
-    while (i < count)
-    {
-        Console.WriteLine(msg);
-        i++;
-    }
-}
-//Method21(count: 4, msg: "Текст новый");
-
-//Вид 3 что то возвращают но ничего не принимают
-
-int Method3()
-{
-    return DateTime.Now.Year;
-}
-// int year = Method3();
-// Console.WriteLine(year);
-
-// Вид 4 что то принимают и что то возвращают
-
-// string Method4(int count, string text)
+// void Method21(string msg, int count)
 // {
 //     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// //Method21(count: 4, msg: "Текст новый");
+
+// //Вид 3 что то возвращают но ничего не принимают
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+// // int year = Method3();
+// // Console.WriteLine(year);
+
+// // Вид 4 что то принимают и что то возвращают
+
+// // string Method4(int count, string text)
+// // {
+// //     int i = 0;
+// //     string result = String.Empty;
+// //         while (i < count)
+// //     {
+// //         result = result + text;
+// //         i++;
+// //     }
+// //     return result;
+// // }
+
+// // string res = Method4(10, "asdf ");
+// // Console.WriteLine(res);
+
+// //цикл со счетчиком for
+
+// string Method5(int count, string text)
+// {
+
 //     string result = String.Empty;
-//         while (i < count)
+//     for (int i = 0; i < count; i++)
 //     {
 //         result = result + text;
-//         i++;
 //     }
 //     return result;
 // }
+// // string res = Method5(10, "asdf ");
+// // Console.WriteLine(res);
 
-// string res = Method4(10, "asdf ");
-// Console.WriteLine(res);
+// //цикл в цикле например табл умножения
 
-//цикл со счетчиком for
+// // for (int i = 2; i <= 10; i++)
+// // {
+// //     for (int j = 2; j < 10; j++)
+// //     {
+// //        Console.WriteLine($"{i} x {j} = {i * j}");
+// //     }
+// //     Console.WriteLine();
+// // }
 
-string Method5(int count, string text)
-{
+// //=====Работа с текстом
+// // Дан текст. В тексте нужно все пробелы заменить чёрточками,
+// // маленькие буквы “к” заменить большими “К”,
+// // а большие “С” маленькими “с”.
+// // Ясна ли задача?
 
-    string result = String.Empty;
-    for (int i = 0; i < count; i++)
-    {
-        result = result + text;
-    }
-    return result;
-}
-// string res = Method5(10, "asdf ");
-// Console.WriteLine(res);
+// string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+//             + "ежели бы вас послали вместо нашего милого Винценгероде,"
+//             + "вы бы взяли приступом согласие прусского короля."
+//             + "Вы та красноречивы. Вы дадите мне чаю?";
 
-//цикл в цикле например табл умножения
-
-// for (int i = 2; i <= 10; i++)
+// string Replace(string text, char oldValue, char newValue)
 // {
-//     for (int j = 2; j < 10; j++)
+//     string result = String.Empty;
+
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
 //     {
-//        Console.WriteLine($"{i} x {j} = {i * j}");
+//         if(text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
 //     }
-//     Console.WriteLine();
+//     return result;
 // }
-
-//=====Работа с текстом
-// Дан текст. В тексте нужно все пробелы заменить чёрточками,
-// маленькие буквы “к” заменить большими “К”,
-// а большие “С” маленькими “с”.
-// Ясна ли задача?
-
-string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
-            + "ежели бы вас послали вместо нашего милого Винценгероде,"
-            + "вы бы взяли приступом согласие прусского короля."
-            + "Вы та красноречивы. Вы дадите мне чаю?";
-
-string Replace(string text, char oldValue, char newValue)
-{
-    string result = String.Empty;
-
-    int length = text.Length;
-    for (int i = 0; i < length; i++)
-    {
-        if(text[i] == oldValue) result = result + $"{newValue}";
-        else result = result + $"{text[i]}";
-    }
-    return result;
-}
 // string newText = Replace(text, ' ','|');
 
 // Console.WriteLine(newText);
