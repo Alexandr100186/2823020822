5d8891b62713e4106a1b5b743971d1c25963ee2e
DESKTOP-LLAL946\devil <mr.alkov@mail.ru>
2022-08-19 02:05:47 +0700

    end25

--- 1.HomeWork/lesson4/hometask25/Program.cs
+++ 1.HomeWork/lesson4/hometask25/Program.cs
@@ -2,3 +2,21 @@
 // и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+Console.Write("Введите число A: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень B: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int Exp(int num1, int num2)
+{
+    int result = 1;
+    for (int i = 1; i <= num2; i++)
+    {
+        result = result * num1;
+    }
+    return result;
+}
+
+int exp = Exp(num1, num2);
+Console.WriteLine("Ответ: " + exp);
