eaa8546db79caed5b46a62ecfbc54f89db5b2168
DESKTOP-LLAL946\devil <mr.alkov@mail.ru>
2022-08-23 12:54:18 +0700

    end36

--- 1.HomeWork/Lesson5/task36/Program.cs
+++ 1.HomeWork/Lesson5/task36/Program.cs
@@ -3,3 +3,30 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+Console.Write($"Введи количество элементов: ");
+int numberElements = Convert.ToInt32(Console.ReadLine()); 
+
+int RandomNumbers(int numberElements, int min, int max)
+  {
+  int[] randomNumbers = new int[numberElements];
+  int sumElements = 0;
+  Console.Write("Задан массив: ");
+    Console.Write("[");
+    for (int i = 0; i <randomNumbers.Length; i++ ){
+      randomNumbers[i] = new Random().Next(min, max);
+
+        if (i < randomNumbers.Length - 1) Console.Write($"{randomNumbers[i]}, ");
+        else Console.Write(randomNumbers[i]);
+
+      if (i % 2 != 1)
+      {
+        sumElements = sumElements + randomNumbers[i];
+      }
+    }
+    Console.Write("]");
+    return sumElements;
+  }
+
+int randomNumbers =  RandomNumbers(numberElements, 1, 10);
+
+Console.WriteLine($"\nСумма элементов, расположенных в нечетных индексах: {randomNumbers}");
