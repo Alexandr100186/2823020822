a616781fe17627e8be0e55c9b62d794644f5e5eb
DESKTOP-LLAL946\devil <mr.alkov@mail.ru>
2022-08-24 18:22:54 +0700

    end39

--- ClassWork/lesson6/task39/Program.cs
+++ ClassWork/lesson6/task39/Program.cs
@@ -4,3 +4,64 @@
 // [1 2 3 4 5] -> [5 4 3 2 1]
 // [6 7 3 6] -> [6 3 7 6]
 
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+
+
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+void ReversArray1(int[] array)
+
+{
+    int size = array.Length;
+    for (int i = 0; i < size / 2; i++)
+    {
+        int temp = array[i];
+        int index2 = size - 1;
+        array[i] = array[size -1 - i];
+        array[size -1 - i] = temp;       
+    }
+}
+
+int[] arr = CreateArrayRndInt(5, 1, 9);
+PrintArray(arr);
+ReversArray1(arr);
+PrintArray(arr);
+
+// void ReversArray(int[] array)
+// {
+//     int size = array.Length;
+//     int index1 = 0;
+//     int index2 = size - 1;
+
+//     while (index1 < index2)
+//     {
+//         int obj = array[index1];
+//         array[index1] = array[index2];
+//         array[index2] = obj;
+
+//         index1++;
+//         index2--;
+//     }
+// }
+
