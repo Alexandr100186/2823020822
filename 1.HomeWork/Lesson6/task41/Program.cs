a5d44f866c642174ed9fa7a41c1daa2d67b93bb8
DESKTOP-LLAL946\devil <mr.alkov@mail.ru>
2022-09-01 17:55:39 +0700

    end410

--- 1.HomeWork/Lesson6/task41/Program.cs
+++ 1.HomeWork/Lesson6/task41/Program.cs
@@ -3,16 +3,43 @@
 // -1, -7, 567, 89, 223-> 3
 
 
-Console.Write("Введите целые числа через пробел: ");
-int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-int count = 0;
+// Console.Write("Введите целые числа через пробел: ");
+// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+// int count = 0;
  
-for (int i = 0; i < arr.Length; i++)
-{
-    if (arr[i] > 0)
-    {
-        count++;
-    }
-}
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     if (arr[i] > 0)
+//     {
+//         count++;
+//     }
+// }
   
-Console.WriteLine($"Больше 0: {count} чисел");
+// Console.WriteLine($"Больше 0: {count} чисел");
+
+Console.Write($"Введи число М(количество чисел): ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] massiveNumbers = new int[m];
+
+void InputNumbers(int m){
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введи {i+1} число: ");
+    massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+
+int Comparison(int[] massiveNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < massiveNumbers.Length; i++)
+  {
+    if(massiveNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+InputNumbers(m);
+
+Console.WriteLine($"Введено чисел больше 0: {Comparison(massiveNumbers)} ");
